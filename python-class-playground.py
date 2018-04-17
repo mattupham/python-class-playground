55938e1f701e672534febb29fc0436a5aa16a3e4
Matt Upham <mupham@twilio.com>
2018-04-17 10:12:14 -0700

    add class variables

--- python-class-playground.py
+++ python-class-playground.py
@@ -1,17 +1,26 @@
 #classes and instances
 class Employee:
 
+	num_of_emps = 0
+	raise_amount = 1.04
+
 	def __init__(self, first, last, pay):
 		self.first = first
 		self.last = last
 		self.pay = pay
 		self.email = first + '.' + last + '@company.com'
 
+		Employee.num_of_emps += 1
+
 	def fullname(self):
 		return '{} {}'.format(self.first, self.last)
 
-emp_1 = Employee('Matt', 'Upham', 10)
+	def apply_raise(self):
+		self.pay = int(self.pay * self.raise_amount)
 
-print(emp_1)
 
-print('Full Name: ', emp_1.fullname())
+emp_1 = Employee('Matt', 'Upham', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+emp_3 = Employee('Trevor', 'Luck', 60000)
+
+print('num of emps: ', Employee.num_of_emps)
